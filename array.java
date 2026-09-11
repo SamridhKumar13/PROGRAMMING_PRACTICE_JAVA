758b5691a193e285156847de8ce8902acaa7835d
Samridh Kumar <samridhkumar05@gmail.com>
2026-09-12 00:09:34 +0530

    arrays

---
+++ array.java
@@ -0,0 +1,12 @@
+
+public class array {
+
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 30};
+        for (int i : arr) {
+            System.out.println(i);
+
+        }
+
+    }
+}
