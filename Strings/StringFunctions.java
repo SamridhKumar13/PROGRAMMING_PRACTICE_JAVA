897ee58c324a1cc35ed6427f0e8eb25450939a83
Samridh Kumar <samridhkumar05@gmail.com>
2026-09-18 23:09:19 +0530

    String functions

---
+++ Strings/StringFunctions.java
@@ -0,0 +1,40 @@
+public class StringFunctions {
+    
+
+    public static void main(){
+
+String str = "Samridh";
+System.out.println( str.length());
+System.out.println(str.charAt(0));
+String name = "SAMRIDH";
+System.out.println(str.equals(name));
+System.out.println(str.equalsIgnoreCase(name));
+ 
+
+//? Empty -> length=0
+//? blank -> empty or sirf Spaces hai String me
+
+String abc= "";
+System.out.println(abc.length());
+System.out.println(abc.isEmpty());
+System.out.println(abc.isBlank());
+
+
+String xyz= "  Samridh  ";
+System.out.println( xyz.trim());
+System.out.println(xyz.toUpperCase());
+System.out.println(xyz.toLowerCase());
+
+
+//? Substring 
+String  bcd =" My name is Samridh Kumar ";
+
+System.out.println(bcd.substring(3,8));
+
+//?  .contains()  ->  check that the given character is belonging or not 
+System.out.println(bcd.contains("i"));
+
+
+
+}
+}
