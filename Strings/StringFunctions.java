1e3f6e4fdb710e1b543ec4a212ae1821deb4fa1a
Samridh Kumar <samridhkumar05@gmail.com>
2026-09-23 22:34:37 +0530

    Update StringFunctions.java

--- Strings/StringFunctions.java
+++ Strings/StringFunctions.java
@@ -15,8 +15,8 @@ System.out.println(str.equalsIgnoreCase(name));
 //? blank -> empty or sirf Spaces hai String me
 
 String abc= "";
-System.out.println(abc.length()); //? used to check the length of the array
-System.out.println(abc.isEmpty()); //? used to check if the string is emppty or not
+System.out.println(abc.length());
+System.out.println(abc.isEmpty());
 System.out.println(abc.isBlank());
 
 
@@ -29,13 +29,15 @@ System.out.println(xyz.toLowerCase());
 //? Substring 
 String  bcd =" My name is Samridh Kumar ";
 //?  SubString is used to print a new string from an existing string 
-
-System.out.println(bcd.substring(3,8)); //? SUB STIRNG STARING A ND ENDING PINTS 
+System.out.println(bcd.substring(3,8));
 
 //?  .contains()  ->  check that the given character is belonging or not 
 System.out.println(bcd.contains("i"));
 
-
+int number = 10;
+String test= String.valueOf(number);//? String.valueof() is used to change the data type 
+System.out.println(number +1);
+System.out.println(test + 1); //? now its a string so it will be concatinated.
 
 }
 }
